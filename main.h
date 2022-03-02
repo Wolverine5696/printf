8877dadc1d71eaf206ced66035a97c0eea2d3b8c
Sam <earthstar412@gmail.com>
2022-03-02 08:13:10 +0000

    sortinghat

--- main.h
+++ main.h
@@ -16,10 +16,14 @@ typedef struct specify
 } string;
 /* printf function*/
 int _printf(const char *format, ...);
-int _putchar(char c);
+int _putchar(char c); /*stdout*/
+
+/*character functions*/
 int print_char(va_list c);
-int print_int(va_list n);
 int print_string(va_list s);
+int print_cent(void); /* print_% */
+
+int print_int(va_list n); /*print integers*/
 
 int _strlen(char *s);
 int _strcmp(char s1, char *s2);
@@ -27,8 +31,9 @@ int _prnthex(va_list *args);
 
 int print_rot13(va_list a);
 int print_binary(va_list a);
-int print_rev_string(va_list a);
+int print_float(va_list a);
 
+void rev_string(char *s)
 int _pow(int x, int y);
 void rev_string(char *s);
 int (*get_op_func(char s))(va_list);
