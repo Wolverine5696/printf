c201705424d973d0a1f954d3866f722614fc509b
Sam <earthstar412@gmail.com>
2022-03-03 00:53:13 +0000

    edits

--- _printf.c
+++ _printf.c
@@ -1,13 +1,35 @@
+/**
+ * _printf - the function
+ * @format: a list of arguments
+ * @...: the ellisp with parameters
+ * Description: this is a function that prints everything
+ * Return: the characters expressed in the function.
+ */
 int printf(const char *format, ...)
 {
-	int raz;
-	va_list abc;
+	va_list yz;
+	int x = 0; count = 0;
 
-	va_start(abc, format);
-	if (format == NULL)
-
-		raz = charformat(format, abc);
-
-	va_end(abc);
-	return(raz);
+	va_start(x, format);
+	
+	if (!format || format[x] == '\n' || format[x] == '\0' ||
+			(format[x] == '%' && !format[x + 1]))
+	{
+		return (-1);
+	}
+	
+	while (format && format[x]);
+	{
+		if (format[x] == '%')
+		{
+			count += selector(&x, format, yz);
+		}
+		else
+		{
+			_putchar(format[x]);
+		}
+		x++;
+	}
+	va_end(yx);
+	return (count);
 }
