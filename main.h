dc9d36c972b487fc66c1e7de882adbeaea1c8afe
Sam <earthstar412@gmail.com>
2022-03-03 00:34:28 +0000

    edits

--- main.h
+++ main.h
@@ -5,36 +5,40 @@
 #include <stdlib.h>
 #include <unistd.h>
 /**
- * struct specify - struct to specifiers
+ * struct specify - struct to specifiers, the token
  * @str: string of a special char
- * @func: function associated with the special char
+ * @f: function associated with the special char
  */
-typedef struct specify
+typedef struct list
 {
 	char *str
-	int (*func)(va_list);
-} string;
+	int (*f)(va_list p);
+} list_t;
 /* printf function*/
 int _printf(const char *format, ...);
+int selector(int *i, const char *format, va_list x);
+
 int _putchar(char c); /*stdout*/
 
 /*character functions*/
-int print_char(va_list c);
-int print_string(va_list s);
-int print_cent(void); /* print_% */
-
-int print_int(va_list n); /*print integers*/
+int str_char(va_list c);
+int str_string(va_list s);
+int str_percent(va_list __attribute__((unused)) p); /* print_% */
+int str_num(va_list arg); /*print integers*/
 
 int _strlen(char *s);
 int _strcmp(char s1, char *s2);
 int _prnthex(va_list *args);
 
-int print_rot13(va_list a);
-int print_binary(va_list a);
-int print_float(va_list a);
-
+/* a few thoughts on the advanced tasks */
+int rot13(va_list a);
+int str_binary(va_list a);
+int str_float(va_list a);
+int str_unassigned(va_list arg);
+int str_octal(va_list p);
 void rev_string(char *s)
 int _pow(int x, int y);
 void rev_string(char *s);
+/* ??? */
 int (*get_op_func(char s))(va_list);
 #endif
