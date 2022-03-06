6290533863f3d864c1334680b2d52e1c63b1363a
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-06 14:27:08 -0800

    added branch deposits

--- main.h
+++ main.h
@@ -1,28 +1,36 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
+#ifndef PRINT_F
+#define PRINT_F
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
+
 /**
- * struct list- struct to specifiers, the token
- * @op: string of a special char
- * @f: function of special char
+ * struct ranger - defines a structure for symbols and functions
+ *
+ * @sym: The operator
+ * @f: The function associated
  */
-typedef struct formats
+struct ranger
 {
 	char *sym;
 	int (*f)(va_list);
-} specifier;
-
+};
+typedef struct ranger range_r;
 /* printf function*/
+int parser(const char *format, range_r f_list[], va_list razor);
 int _printf(const char *format, ...);
-int charsFormats(const char *format, va_list args);
+int charsFormats(const char *format, va_list blue);
 int _strlen(char *s);
 int _putchar(char c); /*stdout*/
 
 /*character functions*/
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
 int _char(va_list c);
 int _string(va_list s);
 int _percent(va_list list); /* print_% */
