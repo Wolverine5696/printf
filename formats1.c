793a41c4500aa0cf7a05c9d8d848ac3dfbc784f0
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-06 22:48:16 -0800

    fixed answer

--- formats1.c
+++ formats1.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
 /**
+ * main - prints out characters in a format string
+ * @c: prints character
+ * @s: prints string
+ * @%: prints percent
+ * @d: prints d integer
+ * @i: prints i integer
  * _printf - prints the main string and the all the integers
  *
  * @format: string with the named characters
@@ -9,13 +15,13 @@
 int _printf(const char *format, ...)
 {
 	int printall;
-	razor1 f_list[] = {
-	{"c", print_char},
-	{"s", print_string},
-	{"%", print_percent},
-	{"d", print_integer},
-	{"i", print_integer},
-	{NULL, NULL}
+	razor_1 f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{NULL, NULL}
 	};
 	va_list blue;
 
