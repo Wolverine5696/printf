54cbe45f019ba58880073c896b9d6a4d0cebd0a0
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-07 00:49:44 -0800

    adding source code

---
+++ formats3.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * get_func - gets the function
+ * @arg: format
+ * @next: next
+ * Return: function that prints the function
+ */
+int get_func(char next, va_list arg)
+{
+	int i;
+
+	flag_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{NULL, NULL}
+	};
+
+	for (i = 0; list[i].mod != NULL; i++)
+	{
+		if (list[i].mod[0] == next)
+			return (list[i].f(arg));
+	}
+	return (0);
+}
