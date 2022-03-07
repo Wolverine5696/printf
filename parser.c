62742fc494f74f30281849ef0f023966a50d7273
root <3249@holbertonschool.com>
2022-03-06 21:27:43 -0800

    betty edits and clean code

--- parser.c
+++ parser.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * parser - Receives the main string and all the necessary parameters to
@@ -8,7 +8,7 @@
  * @arg_list: A list containing all the argumentents passed to the program.
  * Return: A total count of the characters printed.
  */
-int parser(const char *format, razor f_list[], va_list shark)
+int parser(const char *format, razor_1 f_list[], va_list shark)
 {
 	int i, j, r_val, printall;
 
@@ -21,7 +21,7 @@ int parser(const char *format, razor f_list[], va_list shark)
 			for (j = 0; f_list[j].sym != NULL; j++)
 			{
 				if (format[i + 1] == f_list[j].sym[0])
-		                                                                                                                                                                                                              					{
+				{
 					r_val = f_list[j].f(shark);
 					if (r_val == -1)
 						return (-1);
@@ -37,7 +37,8 @@ int parser(const char *format, razor f_list[], va_list shark)
 				_write_char(format[i + 1]);
 				printall = printall + 2;
 			}
-			else															return (-1);
+			else
+				return (-1);
 			}
 			i = i + 1; /*Updating i to skip format symbols*/
 		}
