4b4a24113662bef33f50556602c80249dad78063
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-07 16:03:11 -0800

    editing code

--- formats3.c
+++ formats3.c
@@ -5,22 +5,22 @@
  * @next: next
  * Return: function that prints the function
  */
-int get_func(char next, va_list arg)
+int functio_n(char red, va_list plist)
 {
 	int i;
 	char array[3] = {c, s, % };
 
-		get_func[j] = {
+		functio_n[j] = {
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
 		{NULL, NULL}
 	};
 
-	for (i = 0; get_func[j].mod != NULL; i++)
+	for (i = 0; functio_n[j].mod != NULL; i++)
 	{
-		if (get_func[j].mod[0] == next)
-			return (get_func[j].f(arg));
+		if (functio_n[j].mod[0] == red)
+			return (functio_n[j].f(plist));
 	}
 	return (0);
 }
