1680da703561ea7a61d959a0496760d19b63070b
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-09 02:18:26 -0800

    adding file

---
+++ printfinal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * main - function that prints characters and integers
+ *
+ * @
+ *
+ */
+
+int main ()
+{
+	printf("character: %c\n", 'C');
+	printf("string: %s\n",  "programming is complicated at times");
+	printf("integer: %%\n", '%');	
+	
+		 
+
